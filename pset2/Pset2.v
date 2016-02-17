6d43ef7a62cef511f1068645e9e01ebf1e758fbd
Peng Wang <wangp.thu@gmail.com>
2016-02-17 15:51:07 -0500

    Added pset2

---
+++ pset2/Pset2.v
@@ -0,0 +1,9 @@
+(** * 6.887 Formal Reasoning About Programs, Spring 2016 - Pset 2 *)
+
+Require Import Frap Pset2Sig.
+
+
+Theorem increment2_ok :
+  invariantFor increment2_sys increment2_correct.
+Proof.
+Admitted.
